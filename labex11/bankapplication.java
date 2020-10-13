4a36ee97cc9094baaddb5a2c00f78ee97d474b17
SUBHASHREE-19CSE028 <68490230+SUBHASHREE-19CSE028@users.noreply.github.com>
2020-10-13 13:28:20 +0530

    Add files via upload

---
+++ labex11/bankapplication.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labex11;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author INTEL
+ */ 
+
+
+
+class bankapp
+{
+    Scanner obj=new Scanner(System.in);
+    //getting balance amount
+    double balance=obj.nextDouble();
+    
+    synchronized void withdraw(double amount)
+    { 
+        if(this.balance < amount)
+        {
+             System.out.println("Balance amount in your account...Rs."+balance);
+             System.out.println("Less balance amount,waiting to deposit.....");
+        }
+        try
+        {
+            wait(); 
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println(e);
+        }
+        this.balance-=amount;
+        System.out.println("withdrawal amount.....Rs."+amount);
+        notify();
+        System.out.println("withdraw completed!!!");
+        System.out.println("After the withdrawal,Balance amount in your account...Rs."+balance);
+                
+    }
+    synchronized void deposit(double amount)
+    {
+        System.out.println("Going to deposit.......");
+     
+        if(amount<=40000 && amount>=100)
+               this.balance+=amount;
+        /*try
+        {
+            wait();
+            
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println(e);
+        }*/
+     
+        System.out.println("Deposited amount....Rs."+amount);
+        
+        notify();
+        System.out.println("Deposited successfully!!");
+        System.out.println("After deposited the amount,Balance in your account is..Rs."+balance);
+                
+    }
+    
+}
+class credit implements Runnable
+{
+    bankapp p;
+    credit(bankapp p)
+    {
+        this.p=p;
+        new Thread(this,"deposit").start();
+        /*try
+        {
+            Thread.sleep(6000);
+            
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println(e);
+        }*/
+                
+    }
+    @Override
+    public void run()
+    {
+            double amount;
+            System.out.println("Enter the amount to be credited:");
+            Scanner obj=new Scanner(System.in);
+            amount=obj.nextDouble();
+            p.deposit(amount);
+    }
+    
+}
+class debit implements Runnable
+{
+    bankapp p;
+    debit(bankapp p)
+    {
+        this.p=p;
+        new Thread(this,"withdraw").start();
+        try
+        {
+            Thread.sleep(5000);
+            
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println(e);
+        }
+    }
+    @Override
+    public void run()
+    {
+            double amount;
+            System.out.println("Enter the amount to be debited:");
+            Scanner obj=new Scanner(System.in);
+            amount=obj.nextDouble();
+            p.withdraw(amount);
+       
+    }
+    
+}
+
+public class bankapplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Welcome to banking!!!");
+        System.out.println("Enter the balance amount:");
+        bankapp obj=new bankapp();
+        debit d=new debit(obj);
+        credit c = new credit(obj);
+        
+    }
+    
+}
---
+++ labex11/threadprogram.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labex11;
+import java.util.*;
+/**
+ *
+ * @author INTEL
+ */
+public class threadprogram {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.lang.InterruptedException
+     */
+    public static void main(String[] args) throws InterruptedException {
+        // TODO code application logic here
+        thread1_Nvalues obj=new thread1_Nvalues();
+        thread2_divisibleby5 obj1=new thread2_divisibleby5(obj);
+        thread3_average obj2=new thread3_average(obj);
+    }
+}
+class thread1_Nvalues implements Runnable   //thread1--->getting n values
+{
+    Scanner obj=new Scanner(System.in);
+    
+    int n;
+    int value[],sum;
+    public thread1_Nvalues()
+    {
+       System.out.println("Enter the value of n:");
+       n=obj.nextInt();
+       value=new int[n];
+       System.out.println(n+" values are:");
+       for(int i=0;i<n;i++)
+       {
+            value[i]=obj.nextInt();
+       }
+        
+    }
+    @Override
+    public void run()
+    {
+       this.n=n;
+       new Thread(this).start();
+    }
+}
+class thread2_divisibleby5 extends Thread  //thread2-->divisible by 5
+{
+    thread1_Nvalues n;
+    thread1_Nvalues value[];
+    public thread2_divisibleby5(thread1_Nvalues n)
+    {
+        this.n=n;
+        new Thread(this).start();
+    }
+    @Override
+    public void run()
+    {
+          try
+          {
+            Thread.sleep(6000);
+          }
+          catch(InterruptedException e)
+          {
+            System.out.println(e);
+          }
+          System.out.println("The numbers divisible by 5 are:");
+          for(int i:n.value)
+          {
+             if(i%5==0)
+                 System.out.println(i);
+          
+          }
+    
+    }
+}
+class thread3_average extends Thread   //thread3-->average
+{
+    thread1_Nvalues n; 
+    thread1_Nvalues value[];
+    thread3_average(thread1_Nvalues n)
+    {
+        this.n=n;
+        new Thread(this).start();
+    }
+    @Override
+    public void run()
+    {
+      double sum=0;
+      try
+      {
+         Thread.sleep(8000);
+      }
+      catch(InterruptedException e)
+      {
+         System.out.println(e);
+      }
+   
+      for(int i:n.value)
+      {
+          sum=sum+i; 
+      }
+      double average;
+      average = sum/n.value.length;  //finding average
+      
+      System.out.println("The average of "+n.value.length+" values: "+average);
+    }
+}
