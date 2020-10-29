8481aad1f42945c4785df69d8826175a6c6e5245
SUBHASHREE-19CSE028 <68490230+SUBHASHREE-19CSE028@users.noreply.github.com>
2020-10-29 19:08:26 +0530

    Update registration_form.java

--- labex13/registration_form.java
+++ labex13/registration_form.java
@@ -11,17 +11,12 @@ package labex13;
  */
 import java.awt.Color;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JRadioButton;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 
